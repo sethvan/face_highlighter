8fc66fe95239514f89c620674e5f56093ab16159
Seth Van Hankins <sethvan@yahoo.com>
2023-08-10 11:58:22 -0700

    Removed comnmented out code from main.cpp

--- lib/main.cpp
+++ lib/main.cpp
@@ -13,15 +13,5 @@ using namespace emscripten;
 int main()
 {
 
-    //  seth_tl::stl_data info;
-    //  seth_tl::parse_stl( info, stl_file_name );
-    //  seth_tl::getAdjacentFaces( info.triangles );
-    //  std::vector<seth_tl::Triangle> triangles = info.triangles;
-    //  seth_tl::Point model_center = seth_tl::getCenter( triangles );
-    //  float scaleFactor = seth_tl::getScaleFactor( triangles );
-    //  std::vector<std::vector<seth_tl::Snippet>> selectedIndices;
-    //  selectedIndices.push_back( {} ); // Do not want to use an index 0
-    // getFloat();
-
     return 0;
 }
